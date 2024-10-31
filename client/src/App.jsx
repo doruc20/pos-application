2fdfd72065c87312fb8c9c69e09775dc720fadf4
Dilaver Oruç <dilaveroruc20@gmail.com>
2024-10-31 21:02:42 +0300

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import BillPage from "./pages/BillPage";
 import CartPage from "./pages/CartPage";
 import CustomerPage from "./pages/CustomerPage";
 import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
 import StatisticPage from "./pages/StatisticPage";
-import Login from "./pages/auth/Login";
-
 function App() {
   return (
     <BrowserRouter>
@@ -16,6 +16,7 @@ function App() {
         <Route path="/bills" element={<BillPage />} />
         <Route path="/customers" element={<CustomerPage />} />
         <Route path="/statistic" element={<StatisticPage />} />
+        <Route path="/products" element={<ProductPage />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -12,7 +12,12 @@ const HomePage = () => {
       try {
         const res = await fetch("http://localhost:5000/api/categories/get-all");
         const data = await res.json();
-        setCategories(data);
+        data &&
+          setCategories(
+            data.map((item) => {
+              return { ...item, value: item.title };
+            })
+          );
       } catch (error) {
         console.log(error);
       }
@@ -24,12 +29,12 @@ const HomePage = () => {
   return (
     <>
       <Header />
-      <div className="home px-6 flex md:flex-row flex-col justify-between gap-10 md:pb-0 pb-24">
+      <div className="home px-6 flex md:flex-row flex-col justify-between gap-10 md:pb-0 pb-24 h-screen">
         <div className="categories overflow-auto max-h-[calc(100vh_-_112px)] md:pb-10">
           <Categories categories={categories} setCategories={setCategories} />
         </div>
         <div className="products flex-[8] max-h-[calc(100vh_-_112px)] overflow-y-auto pb-10">
-          <Products />
+          <Products categories={categories} />
         </div>
         <div className="cart-wrapper min-w-[300px] md:-mr-[24px] md:-mt-[24px] border">
           <CartTotals />
